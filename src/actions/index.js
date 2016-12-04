fa1358c1ee1e4d5285e050985c616a19fe5d9cee
Guilherme Quental <guilherme@quent.al>
2016-12-04 10:13:53 -0200

    Add back and delete function

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const FETCH_POSTS = "FETCH_POSTS"
 export const CREATE_POST = "CREATE_POST"
 export const FETCH_POST = "FETCH_POST"
+export const DELETE_POST = "DELETE_POST"
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api"
 const API_KEY = "?key=quental"
@@ -24,7 +25,7 @@ export function createPost(props) {
     payload: request
   }
 }
- export function fetchPost(id) {
+export function fetchPost(id) {
    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
 
    return {
@@ -32,3 +33,12 @@ export function createPost(props) {
      payload: request
    }
  }
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+
+  return {
+    type: DELETE_POST,
+    payload: request
+  }
+}
